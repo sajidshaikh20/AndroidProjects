ae1efb3e656c382cf90f5406e4eac7d0ec76ad4d
sajid shaikh <sajid20shaikh@gmail.com>
2023-06-23 20:51:13 +0530

    adding card in health form

--- app/src/main/java/com/example/androidprojects/healthform/healform_ui.kt
+++ app/src/main/java/com/example/androidprojects/healthform/healform_ui.kt
@@ -1,12 +1,29 @@
 package com.example.androidprojects.healthform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.androidprojects.R
+import com.example.androidprojects.peramids.peramids_riversed
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class healform_ui : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_healform_ui)
+        val btnhealthdata = findViewById<MaterialButton>(R.id.SubmitButton)
+        val patientname = findViewById<EditText>(R.id.UserNameEdt)
+        val patientsurname = findViewById<EditText>(R.id.UserSurNameEdt)
+        val patientmail = findViewById<EditText>(R.id.UserSurEmail)
+        val patientnumber = findViewById<EditText>(R.id.UserContact)
+        val patientdob = findViewById<EditText>(R.id.UserDob)
+
+
+        btnhealthdata.setOnClickListener {
+            val intent = Intent(this, healthdatashow::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/androidprojects/recyclerview/recycleview.kt
+++ app/src/main/java/com/example/androidprojects/recyclerview/recycleview.kt
@@ -8,15 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.androidprojects.R
 import com.example.androidprojects.recyclerview.adapter.cardadapter
 import com.example.androidprojects.recyclerview.model.StudentModel
-
 class recycleview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycleview)
         var UserData = ArrayList<StudentModel>()
-        UserData.add(StudentModel(R.drawable.linkedln_image,"sajid shaik", 25,"Mca" ))
-        UserData.add(StudentModel(R.drawable.shubham,"Shubham verma", 22, "Bba"))
-        UserData.add(StudentModel(R.drawable.shubham,"Harshit jalodiya", 20, "bca"))
+        UserData.add(StudentModel(R.drawable.linkedln_image,"Sajid Shaikh", 25,"Mca" ))
+        UserData.add(StudentModel(R.drawable.main_profile2,"Shubham verma", 22, "Bba"))
+        UserData.add(StudentModel(R.drawable.post2_demo,"Harshit jalodiya", 20, "bca"))
         UserData.add(StudentModel(R.drawable.shubham,"aashish ", 12, "mca"))
         UserData.add(StudentModel(R.drawable.shubham,"verma sir", 30, "Maths"))
         UserData.add(StudentModel(R.drawable.shubham," ayan shaikh", 23, "m tech"))
@@ -24,14 +23,8 @@ class recycleview : AppCompatActivity() {
         UserData.add(StudentModel(R.drawable.shubham,"divya patil", 22, "bcom"))
         UserData.add(StudentModel(R.drawable.shubham,"Shubham verma", 27, "patwari"))
         UserData.add(StudentModel(R.drawable.shubham,"praveen singh", 22, "Fresher"))
-
-
         val recycleview = findViewById<RecyclerView>(R.id.RecycleView)
         recycleview.layoutManager = LinearLayoutManager(this)
         recycleview.adapter = cardadapter(this,UserData)
-
-
-
-
     }
 }
